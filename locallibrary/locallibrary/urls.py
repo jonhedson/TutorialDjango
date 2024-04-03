37e985cd6c416cea3a7e7e2bf2b80bbb1aec22be
Jonh Edson <jonhedson@yahoo.com.br>
2024-04-02 21:43:11 -0300

    Configurando banco de dados, mapeador de urls, timezone e language

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+ 	path('admin/', admin.site.urls),
+ 	path('catalog/', include('catalog.urls')),
+ 	path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
